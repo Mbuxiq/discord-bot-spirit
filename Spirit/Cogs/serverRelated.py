ca0b2498b5d2da306301687b14f93835e6af6613
Mbuxiq <72502318+Mbuxiq@users.noreply.github.com>
2021-05-25 17:28:45 +0200

    Update serverRelated.py

--- Spirit/Cogs/serverRelated.py
+++ Spirit/Cogs/serverRelated.py
@@ -11,7 +11,7 @@ class ServerRelated(commands.Cog):
         self.bot = bot
 
     ##
-    @commands.Cog.listener() ##! <- WORKS HERE BUT NOT IN COGS, WHY?
+    @commands.Cog.listener() 
     async def on_voice_state_update(self, member, before, after):
         if after.channel != None:
             if after.channel.id == 846117723825111110:
@@ -26,18 +26,6 @@ class ServerRelated(commands.Cog):
                     await self.bot.wait_for('voice_state_update', check=check)
                     await channel2.delete()
 
-    @commands.command()
-    async def rules(self, ctx):
-        rulesembed=discord.Embed(title="Rules:", description="[Warnings are point based system. Any violation adds more points to your account. If the points reach a certain value, you will get banned]", color=0x000000)
-        rulesembed.add_field(name="1. Attacking other users is prohibited.", value="[10 ban points]", inline=False)
-        rulesembed.add_field(name="2. Posting NSFW/NSFL is prohibited.", value="[50 ban points]", inline=False)
-        rulesembed.add_field(name="3. Mark loud and possible epilepsy triggers as a spoiler.", value="[20 ban points]", inline=False)
-        rulesembed.add_field(name="4. Spreading drama is prohibited.", value="[10 ban points]", inline=False)
-        rulesembed.add_field(name="5. Self advertising and/or raiding is prohibited.", value="[75 ban points]", inline=False)
-        rulesembed.add_field(name="6. Use channels correctly", value="[5 ban points]", inline=False)
-        rulesembed.add_field(name="7. Abusing the bot is prohibited.", value="[15 ban points]", inline=False)
-        rulesembed.add_field(name="8. Spamming is prohibited.", value="[ (2^spam messages)/2 ban points]", inline=False)
-        await ctx.send(embed= rulesembed)
 
 
 
